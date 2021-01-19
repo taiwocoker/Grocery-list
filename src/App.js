4715d9e4836b7a493bae3275eb033ccff6fa7e33
taiwocoker <taiwofcoker@gmail.com>
2021-01-19 18:31:12 +0100

    Add State to the component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
+  const [name,setName] = useState('');
+  const [list,setList] = useState([]);
+  const [isEditing,setIsEditing] = useState(false)
+  const [editID.setEditID] =useState(null)
+  const [alert,setAlert] = useState({show: false, msg: '',msg: ''})
   return <h2>grocery bud setup</h2>
 }
 
