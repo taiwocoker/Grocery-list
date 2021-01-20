3e5b9a6c53a9c76351e1a66266e8b081a73758f8
taiwocoker <taiwofcoker@gmail.com>
2021-01-20 13:05:55 +0100

    Add edit logic and props to component

--- src/App.js
+++ src/App.js
@@ -3,57 +3,82 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  const [name,setName] = useState('');
-  const [list,setList] = useState([]);
-  const [isEditing,setIsEditing] = useState(false)
-  const [editID,setEditID] =useState(null)
-  const [alert,setAlert] = useState({show: false, msg: '', type: ''})
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(!name){
-      showAlert(true,'danger','please enter value')
-    }else if(name && isEditing) {
-
-    }else{
-      showAlert(true,'success','item added to list')
-      const newItem = {id: new Date().getTime().toString(), title: name};
-      setList([...list,newItem])
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value')
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name }
+          }
+          return item
+        })
+      )
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true,'success','value changed')
+    } else {
+      showAlert(true, 'success', 'item added to list')
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem])
       setName('')
     }
   }
 
-  const showAlert = (show=false,type='',msg='') => {
-    setAlert({show,type,msg})
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg })
   }
-  
+
   const clearList = () => {
-    showAlert(true, 'danger','empty list')
+    showAlert(true, 'danger', 'empty list')
     setList([])
   }
 
   const removeItem = (id) => {
-    showAlert(true,'danger','item removed')
+    showAlert(true, 'danger', 'item removed')
     setList(list.filter((item) => item.id !== id))
   }
-  
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setName(specificItem.title)
+  }
+
   return (
     <section className='section-center'>
       <form className='grocery' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>Grocery List</h3>
         <div className='form-control'>
-          <input type='text' className='grocery' placeholder='e.g cheese' onChange={(e) => setName(e.target.value)} />
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g cheese'
+            onChange={(e) => setName(e.target.value)}
+          />
           <button className='submit-btn' type='submit'>
             {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
       {list.length > 0 && (
-      <div className='grocery-container'>
-        <List items={list} removeItem={removeItem}/>
-        <button className='clear-btn'onClick={clearList}>Clear items</button>
-      </div>
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            Clear items
+          </button>
+        </div>
       )}
     </section>
   )
--- src/List.js
+++ src/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({ items }) => {
+const List = ({ items,removeItem,editItem }) => {
   return (
     <div className='grocery-list'>
       {items.map((item) => {
@@ -10,10 +10,10 @@ const List = ({ items }) => {
             <p className='title'>{title}</p>
             <div className='btn-container'>
               <button type='button' className='edit-btn'>
-                <FaEdit />
+                <FaEdit onClick={() => editItem(id)}/>
               </button>
               <button type='button' className='delete-btn'>
-                <FaTrash />
+                <FaTrash onClick={() => removeItem(id)}/>
               </button>
             </div>
           </article>
