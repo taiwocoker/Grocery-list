f1c082fdf1d3e3a2dbda72925aa8ce09104adc9e
taiwocoker <taiwofcoker@gmail.com>
2021-01-20 12:08:15 +0100

    Add props to alert component

--- src/App.js
+++ src/App.js
@@ -12,19 +12,25 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault()
     if(!name){
-
+      showAlert(true,'danger','please enter value')
     }else if(name && isEditing) {
 
     }else{
       const newItem = {id: new Date().getTime().toString(), title: name};
-      setList(...list,newItem)
+      setList([...list,newItem])
       setName('')
     }
   }
+
+  const showAlert = (show=false,type='',msg='') => {
+    setAlert({show,type,msg})
+  }
+  
+  
   return (
     <section className='section-center'>
       <form className='grocery' onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
         <h3>Grocery bud</h3>
         <div className='form-control'>
           <input type='text' className='grocery' placeholder='e.g cheese' onChange={(e) => setName(e.target.value)} />
@@ -33,10 +39,12 @@ function App() {
           </button>
         </div>
       </form>
+      {list.length > 0 && (
       <div className='grocery-container'>
         <List items={list}/>
         <button className='clear-btn'>Clear items</button>
       </div>
+      )}
     </section>
   )
 }
